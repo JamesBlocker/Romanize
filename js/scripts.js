a5b0cbcfaeb7f20e65f80f78a29dbbd4576e9a16
Byron <byronblocker@gmail.com>
2017-09-20 16:19:27 -0700

    add backend logic to translate into numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-var romanize = function(input) {
-  return input;
+function romanize(number) {
+  var numerals = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1},
+      roman = '',
+      i;
+  for (i in numerals) {
+    while (number >= numerals[i]) {
+      roman += i;
+      number -= numerals[i];
+      alert('number:' + number + ' - roman:' + roman );
+    }
+
+  }
+  return roman;
 };
 
 $(document).ready(function() {
